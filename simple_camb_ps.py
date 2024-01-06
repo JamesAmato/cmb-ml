2bec5b4b4387c1a4ea60adcb63af7df6d31ca318
James Amato <116766236+JamesAmato@users.noreply.github.com>
2024-01-05 22:39:01 -0600

    intial class: make ps from via params

--- simple_camb_ps.py
+++ simple_camb_ps.py
@@ -1,61 +1,44 @@
-import os
 import numpy as np
-import pysm3
-import healpy as hp
 import camb
 import matplotlib.pyplot as plt
-import pysm3.units as u
 
 
-def setup_camb(cosmo_params):
-    pars = camb.CAMBparams()
+class SimpleCAMBfromWMAP:
+    def __init__(self, ellmax, cosmo_params):
+        self.pars = camb.CAMBparams()
 
-    # Original from https://camb.readthedocs.io/en/latest/CAMBdemo.html
-    # pars.set_cosmology(H0=67.4, ombh2=0.0224, omch2=0.120, mnu=0.06, omk=0.001, tau=0.054)
-    # pars.InitPower.set_params(As=2e-9, ns=0.965, r=0)
-    # pars.set_for_lmax(ellmax, lens_potential_accuracy=0)
+        # Original from https://camb.readthedocs.io/en/latest/CAMBdemo.html
+        # pars.set_cosmology(H0=67.4, ombh2=0.0224, omch2=0.120, mnu=0.06, omk=0.001, tau=0.054)
+        # pars.InitPower.set_params(As=2e-9, ns=0.965, r=0)
+        # pars.set_for_lmax(ellmax, lens_potential_accuracy=0)
 
-    pars.set_cosmology(H0=cosmo_params["H0"], 
-                       ombh2=cosmo_params["omegabh2"], 
-                       omch2=cosmo_params["omegach2"], 
-                       tau=cosmo_params["tau"]
-                       )
-    pars.InitPower.set_params(
-                              As=cosmo_params["As"], 
-                              ns=cosmo_params["ns002"]
-                              )
-    return pars
+        self.pars.set_cosmology(H0=cosmo_params["H0"], 
+                        ombh2=cosmo_params["omegabh2"], 
+                        omch2=cosmo_params["omegach2"], 
+                        tau=cosmo_params["tau"]
+                        )
+        self.pars.InitPower.set_params(
+                                As=cosmo_params["As"], 
+                                ns=cosmo_params["ns002"]
+                                )
 
+        self.pars.set_for_lmax(ellmax, lens_potential_accuracy=0)
+        self.results = camb.get_results(self.pars)
+        self.all_ps = self.results.get_cmb_power_spectra(self.pars, CMB_unit='muK')
+        self.ps = self.all_ps['total']
 
-def run_camb(pars, ellmax):
-    pars.set_for_lmax(ellmax, lens_potential_accuracy=0)
-    results = camb.get_results(pars)
-    return results
-
-
-def write_cls(filename, camb_results):
-    camb_results.save_cmb_power_spectra(filename=filename)
+    def write_cls(self, filename):
+        self.results.save_cmb_power_spectra(filename=filename)
 
 
-def main():
-    ellmax=2000
-    cosmo_params = dict(
-        H0=67.4,
-        omegabh2=0.0224,
-        omegach2=0.120,
-        As=2e-9,
-        ns002=0.965,
-        tau=0.054
-    )
-    camb_params = setup_camb(cosmo_params)
-    camb_results = run_camb(camb_params, ellmax=ellmax)
-    ps = camb_results.get_cmb_power_spectra(camb_params, CMB_unit='muK')
+def show_camb_results(simple_camb):
+    ps = simple_camb.all_ps
 
-    totCL = ps['total']
+    totCL = simple_camb.ps
     unlensedCL = ps['unlensed_scalar']
 
     ls = np.arange(totCL.shape[0])
-    fig, ax = plt.subplots(2,2, figsize = (12,12))
+    fig, ax = plt.subplots(2,2, figsize = (6,6))
     ax[0,0].plot(ls,totCL[:,0], color='k')
     ax[0,0].plot(ls,unlensedCL[:,0], color='C2')
     ax[0,0].set_title(r'$TT\, [\mu K^2]$')
@@ -68,6 +51,24 @@ def main():
     ax[1,1].plot(ls,unlensedCL[:,3], color='C2')
     ax[1,1].set_title(r'$TE\, [\mu K^2]$')
     plt.show()
+
+
+def main():
+    ellmax=2000
+    cosmo_params = dict(
+        H0=67.4,
+        omegabh2=0.0224,
+        omegach2=0.120,
+        As=2e-9,
+        ns002=0.965,
+        tau=0.054
+    )
+    simple_camb = SimpleCAMBfromWMAP(ellmax=ellmax,
+                                     cosmo_params=cosmo_params)
+    simple_camb2 = SimpleCAMBfromWMAP(ellmax=ellmax,
+                                     cosmo_params=cosmo_params)
+    a = simple_camb.ps - simple_camb2.ps
+    print(np.max(a))
 
 
 if __name__ == "__main__":
