ddcd9698b3b4b1b86eeac4cd5e95908103929713
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2024-01-11 19:17:57 -0600

    tested

--- cmb_component.py
+++ cmb_component.py
@@ -1,13 +1,18 @@
-import os
-import numpy as np
-import pysm3
-import healpy as hp
+import tempfile
+import inspect
+from pysm3 import CMBLensed
 import camb
-import matplotlib.pyplot as plt
+
+# For test code
+import healpy as hp
+import pysm3
 import pysm3.units as u
+import matplotlib.pyplot as plt
+from planck_cmap import colombi1_cmap
 
 
 # Based on https://camb.readthedocs.io/en/latest/CAMBdemo.html
+# TODO: Break out test code into separate file
 
 
 def get_cls(ellmax):
@@ -45,90 +50,139 @@ def write_cls(ellmax):
     return filename
 
 
-# class CMBMapFromCl(pysm3.Model):
-#     """Synthesizes CMB maps from input Cls"""
+def create_cmb_lensed_from_params(cosmo_params, 
+                                  lmax, 
+                                  nside,
+                                  cmb_ps_file_out=None,
+                                  max_nside=None,
+                                  cmb_seed=None, 
+                                  apply_delens=False,
+                                  delensing_ells=None,
+                                  map_dist=None
+                                  ) -> CMBLensed:
+    # TODO Clean up documentation
+    """Returns a pysm3 CMBLensed object
 
-#     def __init__(
-#         self, 
-#         nside, 
-#         cl: np.ndarray,
-#         seed: int,
-#         max_nside=None,
-#         map_dist=None
-#     ):
-#         """
-#         Class template source is PySM3 CMBMap
-#         The input is assumed to be in `uK_CMB`
+    Documentation from there
 
-#         Parameters
-#         ----------
-#         nside: int
-#             HEALPix N_side parameter of the maps to create
-#         cls: `np.ndarray` object
-#             The cls to use for creating a CMB realization.
-#             Expects an array shape (maxell, 4), in the order TT, 
-#         seed: int
-#             healpy changes numpy's random seed in order to control realizations
-#         max_nside: int
-#             Keeps track of the the maximum Nside this model is available at
-#             by default 512 like PySM 2 models. See 
-#             https://pysm3.readthedocs.io/en/latest/index.html#best-practices-for-model-execution
-#             Unsure how to set this; leaving as-is for now.
-#         map_IQU: `pathlib.Path` object
-#             Path to a single IQU map
-#         """
-#         # TODO: Set max_nside to reasonable value?
-#         # TODO: Figure out how to do MPI stuff with hp synalm and alm2map (?)
-#         if max_nside is None:
-#             max_nside = nside
-#         super().__init__(nside=nside, max_nside=max_nside, map_dist=map_dist)
-#         if not isinstance(cl, np.ndarray):
-#             raise TypeError("cl must be a numpy array, shape (maxell, 4)")
-#         if cl.shape[1] != 4:
-#             raise ValueError("cl must be a numpy array shape (maxell, 4)")
-#         self.cl = cl
-#         self.seed = seed
-#         self.map = CMBMapFromCl.make_map(cl, seed, nside)
+    Takes an input unlensed CMB and lensing spectrum from CAMB and uses
+    Taylens to apply lensing, it optionally simulates delensing by
+    suppressing the lensing power at specific scales with the user
+    provided `delensing_ells`.
 
-#     @staticmethod
-#     def make_map(cl, seed, nside):
-#         # TODO: Fix healpy seed method (somehow?) (update healpy's function?)
-#         np.random.seed(seed)
-        
-#         # TODO: Make synalm flexible, less python is better (update healpy's function?)
-#         # Convert to a list of four numpy arrays
-#         cl = list(cl.T)
+    Parameters
+    ----------
 
-#         alm = hp.synalm(cl, new=False)
-#         cmb_map = hp.alm2map(alm, nside=nside)
-#         # TODO: Is unit=u.uK_CMB correct? It's a guess based on CMBMap
-#         return u.Quantity(cmb_map, unit=u.uK_CMB)
+    cosmo_params : dict(str: float)
+        Cosmological parameters to use
+    :param lmax: :math:`\ell_{\rm max}` you want
+    nside: int
+        Resolution parameter at which this model is to be calculated.
+    cmb_ps_file_out: 
+        Filename to save
+    max_nside: int
+        Keeps track of the the maximum Nside this model is available at
+        by default 512 like PySM 2 models
+    cmb_seed : int
+        Numpy random seed for synfast, set to None for a random seed
+    apply_delens : bool
+        If true, simulate delensing with taylens
+    delensing_ells : path
+        Space delimited file with ells in the first columns and suppression
+        factor (1 for no suppression) in the second column
+    map_dist : bool
+        Something to do with MPI. No PySM3 Documentation in version used.
+    """
 
-#     @u.quantity_input
-#     def get_emission(self, freqs: u.GHz, weights=None) -> u.uK_RJ:
-#         freqs = pysm3.utils.check_freq_input(freqs)
-#         # do not use normalize weights because that includes a transformation
-#         # to spectral radiance and then back to RJ
-#         if weights is None:
-#             weights = np.ones(len(freqs), dtype=np.float32)
+    #Set up a new set of parameters for CAMB
+    pars = camb.CAMBparams()
 
-#         scaling_factor = pysm3.utils.bandpass_unit_conversion(
-#             freqs * u.GHz, weights, output_unit=u.uK_RJ, input_unit=u.uK_CMB
-#         )
+    # I don't know what CAMB is doing, I just know I've got some lump of parameters to 
+    #   plug in to two different functions, so I read signatures to split the params up
+    set_cosmology_params = get_param_names(pars.set_cosmology)
+    init_power_params = get_param_names(pars.InitPower.set_params)
 
-#         # a = self.map * scaling_factor
-#         # b = u.Quantity(a, unit=u.uK_RJ, copy=False)
+    # Split the cosmo_params dictionary
+    set_cosmology_args = {k: v for k, v in cosmo_params.items() if k in set_cosmology_params}
+    init_power_args = {k: v for k, v in cosmo_params.items() if k in init_power_params}
 
-#         return u.Quantity(self.map * scaling_factor, unit=u.uK_RJ, copy=False)
+    # Set the parameters
+    pars.set_cosmology(**set_cosmology_args)
+    pars.InitPower.set_params(r=0, **init_power_args)
 
+    # pars.set_cosmology(H0=67.4, ombh2=0.0224, omch2=0.120, mnu=0.06, omk=0.001, tau=0.054)
+    # pars.InitPower.set_params(As=2e-9, ns=0.965, r=0)
+    pars.set_for_lmax(lmax, lens_potential_accuracy=0)
+    
+    results = camb.get_results(pars)
 
-# def main():
-#     cl = get_cls()
-#     cmb_map = make_map(cl, 512)
-#     print(cmb_map.shape)
-#     hp.mollview(cmb_map[0])
-#     plt.show()
+    def get_cmb_lensed(fn):
+        return CMBLensed(nside=nside,
+                         cmb_spectra=fn,
+                         max_nside=max_nside,
+                         cmb_seed=cmb_seed,
+                         apply_delens=apply_delens,
+                         delensing_ells=delensing_ells,
+                         map_dist=map_dist)
+
+    if cmb_ps_file_out is None:
+        with tempfile.NamedTemporaryFile(mode='w+', delete=True) as tmp_file:
+            results.save_cmb_power_spectra(filename=tmp_file)
+            tmp_file.flush()
+            cmb_lensed = get_cmb_lensed(tmp_file.name)
+    else:
+        results.save_cmb_power_spectra(filename=cmb_ps_file_out)
+        cmb_lensed = get_cmb_lensed(cmb_ps_file_out)
+
+    return cmb_lensed
 
 
-# if __name__ == "__main__":
-#     main()
+# Janky way of handling not knowing what's what in CAMB
+# TODO: Remove the need for this
+def get_param_names(method):
+    sig = inspect.signature(method)
+    return [param.name for param in sig.parameters.values() if param.name != 'self']
+
+
+def try_create_cmb_lensed_from_params():
+    nside = 512
+    lmax = 8150
+    cmb_seed = 0
+    # cmb_ps_file_to_save = "test_ps.txt"
+    cmb_ps_file_to_save = None
+
+    # From planck_deltabandpass.tbl
+    detector_frq = 100.89 * u.GHz
+    fwhm = 9.682 * u.arcmin
+
+    # Params from camb demo file https://camb.readthedocs.io/en/latest/CAMBdemo.html
+    params = dict(H0=67.4, 
+                  ombh2=0.0224, 
+                  omch2=0.120, 
+                  mnu=0.06, 
+                  omk=0.001, 
+                  tau=0.054,
+                  As=2e-9,
+                  ns=0.965
+                  )
+    cmb = create_cmb_lensed_from_params(cosmo_params=params,
+                                        cmb_ps_file_out=cmb_ps_file_to_save,
+                                        lmax=lmax,
+                                        nside=nside,
+                                        cmb_seed=cmb_seed,
+                                        apply_delens=False
+                                        )
+    sky = pysm3.Sky(nside=nside, component_objects=[cmb], output_unit="uK_RJ")
+    skymaps = sky.get_emission(detector_frq)
+    skymap = skymaps[0]
+    map_smoothed = pysm3.apply_smoothing_and_coord_transform(skymap, fwhm=fwhm)
+
+    hp.mollview(map_smoothed, min=-300, max=300,
+                title=f"Test Map {detector_frq}", 
+                unit=map_smoothed.unit,
+                cmap=colombi1_cmap)
+    plt.show()
+
+
+if __name__ == "__main__":
+    try_create_cmb_lensed_from_params()
