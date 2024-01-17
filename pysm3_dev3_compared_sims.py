1eac5ee13ede95b7dd943d251994494bfbe0565e
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2024-01-17 16:24:48 -0600

    added workstation times

--- pysm3_dev3_compared_sims.py
+++ pysm3_dev3_compared_sims.py
@@ -28,16 +28,19 @@ rg#:   [1]     Radio Galaxies
 
 def simulate_sky():
     nside = 128
+    print("Starting Low Complexity. Takes 86s on Lab Workstation.")
     start_time = time()
     # Low complexity contaminants + extragalactic
     sky = pysm3.Sky(nside=nside, preset_strings=["c1", "d9", "s4", "f1", "a1", "co1", "cib1", "ksz1", "tsz1", "rg1"])
     skymap = sky.get_emission(100 * u.GHz)
     print(f"Time for low complexity sky: {time() - start_time:.1f}")
+    print("Starting Medium Complexity. Takes 86s on Lab Workstation.")  # No typo, same as Low Complexity
     start_time = time()
     # Medium complexity contaminants + extragalactic
     sky = pysm3.Sky(nside=nside, preset_strings=["c1", "d10", "s5", "f1", "a1", "co3", "cib1", "ksz1", "tsz1", "rg1"])
     skymap = sky.get_emission(100 * u.GHz)
     print(f"Time for medium complexity sky: {time() - start_time:.1f}")
+    print("Starting High Complexity. Takes 152s on Lab Workstation.")
     start_time = time()
     # High complexity contaminants + extragalactic
     sky = pysm3.Sky(nside=nside, preset_strings=["c1", "d12", "s7", "f1", "a2", "co3", "cib1", "ksz1", "tsz1", "rg1"])
