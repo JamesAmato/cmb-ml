5e9027d3905dd1307cd4537d211a823bc86183cd
James Amato <116766236+JamesAmato@users.noreply.github.com>
2024-01-20 20:40:12 -0600

    no more passing conf around

--- hydra_test5_objects_testhelper.py
+++ hydra_test5_objects_testhelper.py
@@ -34,28 +34,40 @@ class DatasetFileLocator(FileLocator):
         self.dataset_root = Path(conf.local_system.datasets_root)
         self.dataset_name = conf.dataset_name
         self.detectors = conf.simulation.detector_freqs
-        self.splits = [split for split in conf.splits]
+
+        # For use in SplitFolderLocator
+        self.split_structures = dict(conf.splits)
+
+        # For use in SimFolderLocator
+        self.sim_folder_prefix = conf.file_system.sim_folder_prefix
+        self.sim_str_num_digits = conf.file_system.sim_str_num_digits
+        self.sim_loc_structure = conf.file_system.structure
+
+        self.cmb_map_fid_fn = conf.file_system.cmb_map_fid_fn
+        self.cmb_ps_fid_fn = conf.file_system.cmb_ps_fid_fn
+        self.cmb_ps_der_fn = conf.file_system.cmb_ps_der_fn
+        self.obs_map_fn = conf.file_system.obs_map_fn
 
     def get_split(self, split):
         try: 
-            assert split in self.splits
+            assert split in self.split_structures.keys()
         except AssertionError:
             raise ValueError(f"Split {split} is not specified in configuration files.")
-        return SplitFileLocator(self.conf, parent_dfl=self, split_name=split)
+        return SplitFolderLocator(parent_dfl=self, split_name=split)
 
 
-class SplitFileLocator(FileLocator):
+class SplitFolderLocator(FileLocator):
     def __init__(self,
-                 conf: DictConfig, 
                  parent_dfl: DatasetFileLocator, 
                  split_name: str) -> None:
         logger.info(f"Running {self.__class__.__name__} in {__name__}")
         super().__init__(parent_dfl.create_missing_dest)
-        self.conf = conf
         self.dfl = parent_dfl
         self.split_name = split_name
-        self.ps_fidu_fixed = conf.splits[split_name].ps_fidu_fixed
-        self.n_sims = conf.splits[split_name].n_sims
+
+        split_structure = self.dfl.split_structures[split_name]
+        self.ps_fidu_fixed = split_structure.ps_fidu_fixed
+        self.n_sims = split_structure.n_sims
 
     def get_sim(self, sim_num):
         try:
@@ -66,32 +78,36 @@ class SplitFileLocator(FileLocator):
             assert sim_num < self.n_sims
         except AssertionError:
             raise ValueError(f"Sim {sim_num} is outside the range (max: {self.n_sims}) specified in configuration files.")
-        return SimFileLocator(self.conf, parent_sfl=self, sim_num=sim_num)
+        return SimFileLocator(parent_sfl=self, sim_num=sim_num)
 
 
 class SimFileLocator(FileLocator):
     def __init__(self,
-                 conf: DictConfig, 
                  parent_sfl: DatasetFileLocator, 
                  sim_num: int) -> None:
         logger.info(f"Running {self.__class__.__name__} in {__name__}")
         super().__init__(create_missing_dest=parent_sfl.create_missing_dest)
         self.sfl = parent_sfl
-        sim_folder_prefix = conf.file_system.sim_folder_prefix
-        str_num_digits = conf.file_system.sim_str_num_digits
+        self.dfl = parent_sfl.dfl
+
+        sim_folder_prefix = self.dfl.sim_folder_prefix  #
+        str_num_digits = self.dfl.sim_str_num_digits    #
+
         sim_path_build_dict = dict(
-            dataset_name = parent_sfl.dfl.dataset_name,
-            split_name = parent_sfl.split_name,
+            dataset_name = self.dfl.dataset_name,
+            split_name = self.sfl.split_name,
             sim_folder = f"{sim_folder_prefix}{sim_num:0{str_num_digits}d}"
         )
-        sim_loc_path = conf.file_system.structure.format(**sim_path_build_dict)
-        self.sim_path = parent_sfl.dfl.dataset_root / sim_loc_path
-        self.ps_fidu_fixed = parent_sfl.ps_fidu_fixed
 
-        self.cmb_map_fid_fn = conf.file_system.cmb_map_fid_fn
-        self.cmb_ps_fid_fn = conf.file_system.cmb_ps_fid_fn
-        self.cmb_ps_der_fn = conf.file_system.cmb_ps_der_fn
-        self.obs_map_fn = conf.file_system.obs_map_fn
+        sim_loc_path = self.dfl.sim_loc_structure.format(**sim_path_build_dict)  #
+        
+        self.sim_path = self.dfl.dataset_root / sim_loc_path
+        self.ps_fidu_fixed = self.sfl.ps_fidu_fixed
+
+        self.cmb_map_fid_fn = self.dfl.cmb_map_fid_fn
+        self.cmb_ps_fid_fn = self.dfl.cmb_ps_fid_fn
+        self.cmb_ps_der_fn = self.dfl.cmb_ps_der_fn
+        self.obs_map_fn = self.dfl.obs_map_fn
 
     @property
     def cmb_map_fid_path(self):
