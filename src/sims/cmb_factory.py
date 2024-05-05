b83e4e1d7b64898e27eec74d0ae3489aeeb91a9a
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2024-05-05 17:25:37 -0500

    separated seed stuff from cmb_factory, added seed logger

---
+++ src/sims/cmb_factory.py
@@ -0,0 +1,40 @@
+import logging
+
+from pathlib import Path
+from pysm3 import CMBLensed
+from omegaconf.errors import ConfigAttributeError
+from ..core import Asset
+
+
+logger = logging.getLogger(__name__)
+
+
+class CMBFactory:
+    def __init__(self, conf, make_ps_if_absent=None):
+        self.nside = conf.simulation.nside_sky
+        self.max_ell_for_camb = conf.simulation.cmb.ell_max
+        self.wmap_param_labels = conf.simulation.cmb.wmap_params
+        self.camb_param_labels = conf.simulation.cmb.camb_params_equiv
+        self.max_nside_pysm_component = None
+        self.apply_delens = False
+        self.delensing_ells = None
+        self.map_dist = None
+        if make_ps_if_absent is None:
+            try:
+                self.make_ps_if_absent = conf.simulation.cmb.make_ps_if_absent
+            except ConfigAttributeError as e:
+                logger.error(f"CMBMaker needs either a make_ps_if_absent flag in the cmb " \
+                             f"configuration yaml OR a make_ps_if absent argument to init.")
+                logger.exception(e)
+                raise e
+    
+    def make_cmb_lensed(self, seed, powerspectrum: Asset) -> CMBLensed:
+        logger.debug(f'Seed used for CMB: {seed}.')
+        cmb_ps_fid_path = powerspectrum.path
+        return CMBLensed(nside=self.nside,
+                         cmb_spectra=cmb_ps_fid_path,
+                         cmb_seed=seed,
+                         max_nside=self.max_nside_pysm_component,
+                         apply_delens=self.apply_delens,
+                         delensing_ells=self.delensing_ells,
+                         map_dist=self.map_dist)
--- src/sims/random_seed_manager.py
+++ src/sims/random_seed_manager.py
@@ -1,46 +1,16 @@
 
-from typing import List, Dict, Any
+from typing import List
 import logging
 from hashlib import sha256
 from omegaconf import DictConfig
-from pathlib import Path
-from pysm3 import CMBLensed
 
 from omegaconf.errors import ConfigAttributeError
 
-from ..core import Split, Asset
+from ..core import Split
 
-logger = logging.getLogger(__name__)
 
-class CMBFactory:
-    def __init__(self, conf, make_ps_if_absent=None):
-        self.nside = conf.simulation.nside_sky
-        self.max_ell_for_camb = conf.simulation.cmb.ell_max
-        self.wmap_param_labels = conf.simulation.cmb.wmap_params
-        self.camb_param_labels = conf.simulation.cmb.camb_params_equiv
-        self.max_nside_pysm_component = None
-        self.apply_delens = False
-        self.delensing_ells = None
-        self.map_dist = None
-        if make_ps_if_absent is None:
-            try:
-                self.make_ps_if_absent = conf.simulation.cmb.make_ps_if_absent
-            except ConfigAttributeError as e:
-                logger.error(f"CMBMaker needs either a make_ps_if_absent flag in the cmb " \
-                             f"configuration yaml OR a make_ps_if absent argument to init.")
-                logger.exception(e)
-                raise e
-    
-    def make_cmb_lensed(self, seed, powerspectrum: Asset) -> CMBLensed:
-        logger.debug(f'Seed used for CMB: {seed}.')
-        cmb_ps_fid_path = powerspectrum.path
-        return CMBLensed(nside=self.nside,
-                         cmb_spectra=cmb_ps_fid_path,
-                         cmb_seed=seed,
-                         max_nside=self.max_nside_pysm_component,
-                         apply_delens=self.apply_delens,
-                         delensing_ells=self.delensing_ells,
-                         map_dist=self.map_dist)
+logger = logging.getLogger('seed_logger')
+
 
 class SeedMaker:
     def __init__(self, 
