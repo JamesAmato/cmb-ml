51213ebfb55407832c82b53716849fe1c8ab8839
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2024-05-05 19:07:47 -0500

    cleaned log entries

--- src/sims/stage_executors/E_make_simulations.py
+++ src/sims/stage_executors/E_make_simulations.py
@@ -54,23 +54,22 @@ class SimCreatorExecutor(BaseStageExecutor):
         # seed maker objects
         self.cmb_seed_factory = SimLevelSeedFactory(cfg, 'cmb')
         self.noise_seed_factory = FieldLevelSeedFactory(cfg, 'noise')
-        # Initialize constants from configs
 
+        # Initialize constants from configs
         self.nside_out = self.experiment.nside
         self.nside_sky = cfg.simulation.nside_sky
-        
+        self.planck_freqs = self.experiment.detector_freqs
+        self.field_strings = self.experiment.map_fields
         preset_strings = list(cfg.simulation.preset_strings)
-        # self.planck_freqs = self.experiment.detector_freqs
-        # self.field_strings = self.experiment.map_fields
         self.lmax_pysm3_smoothing = int(cfg.simulation.cmb.derived_ps_nsmax_x * self.nside_out)
 
         self.placeholder = pysm3.Model(nside=self.nside_sky, max_nside=self.nside_sky)
-        logger.debug('creating sky')
+        logger.debug('Creating PySM3 Sky object')
         self.sky = pysm3.Sky(nside=self.nside_sky,
                             component_objects=[self.placeholder],
                             preset_strings=preset_strings,
                             output_unit='K_CMB')
-        logger.debug('done creating sky')
+        logger.debug('Done creating PySM3 Sky object')
         self.cmb_factory = CMBFactory(cfg)
 
     def execute(self) -> None:
@@ -89,46 +88,30 @@ class SimCreatorExecutor(BaseStageExecutor):
             with self.name_tracker.set_context("sim_num", sim):
                 logger.debug(f"Creating simulation {split.name}:{sim}")
                 cmb_seed = self.cmb_seed_factory.get_seed(split, sim)
-                
-                logger.debug(f"Making CMB")
                 cmb = self.cmb_factory.make_cmb_lensed(cmb_seed, ps)
-                logger.debug(f"Done making CMB")
-
-                logger.debug(f"Hotswapping CMB in sky for {sim}")
                 self.sky.components[0] = cmb
-                logger.debug(f"Done hotswapping CMB in sky for {sim}")
-
                 self.save_cmb_map_realization(cmb, self.out_cmb_map_fid)
 
                 for freq in self.planck_freqs:
                     detector = make_detector(self.deltabandpass, freq)
-
-                    logger.debug(f"for {sim}, {freq} GHz, getting sky emission")
                     skymaps = self.sky.get_emission(detector.cen_freq)
-                    logger.debug(f"for {sim}, {freq} GHz, getting sky emission done")
 
                     obs_map = []
                     for skymap, field_str in zip(skymaps, self.field_strings):
                         if freq in [545, 857] and field_str != "T":
-                            logger.debug(f"Skipping {freq} GHz, {field_str}: source data not found at frequency.")
                             continue
-                        
-                        logger.debug(f'for {sim}, {freq} GHz, {field_str}: applying beam effects')
                         map_smoothed = pysm3.apply_smoothing_and_coord_transform(skymap, 
                                                                                  detector.fwhm, 
                                                                                  lmax=self.lmax_pysm3_smoothing, 
                                                                                  output_nside=self.nside_out)
-                        logger.debug(f'for {sim}, {freq} GHz, {field_str}: making noise')
                         noise_seed = self.noise_seed_factory.get_seed(split, sim, freq, field_str)
                         noise_map = self.get_noise_map(freq, field_str, noise_seed)
                         final_map = map_smoothed + noise_map
                         obs_map.append(final_map)
-                        logger.debug(f"for {sim}, {freq} GHz, {field_str}: done with field")
 
-                    logger.debug(f"for {sim}, {freq} GHz: writing file")
                     self.out_sims.write({str(freq): obs_map})
-                    logger.info(f"for {sim}, {freq} GHz: done with channel")
-                logger.debug(f"for {sim}: done with simulation")
+                    logger.debug(f"For {split.name}:{sim}, {freq} GHz: done with channel")
+                logger.debug(f"For {split.name}:{sim}, done with simulation")
 
     def save_cmb_map_realization(self, cmb: CMBLensed, asset: Asset):
         cmb_realization: Quantity = cmb.map
@@ -137,14 +120,14 @@ class SimCreatorExecutor(BaseStageExecutor):
         nside_out = self.nside_out
         scaled_map = change_nside_of_map(cmb_realization_data, nside_out)
         scaled_map = scaled_map * cmb_realization_units
-        logger.debug(f"Saving fiducial cmb_map for {asset.path.parent.name}")
+        # logger.debug(f"For {self.name_tracker.context['split']}:{self.name_tracker.context['sim_num']}, saving fiducial cmb_map")
         # TODO: Get answer of asset handler healpymap vs sim.write_fid_map
         asset.write(scaled_map)
 
     def get_noise_map(self, freq, field_str, noise_seed, center_frequency=None):
         with self.name_tracker.set_context('freq', freq):
             with self.name_tracker.set_context('field', field_str):
-                logger.debug(f"Getting noise map for {freq} GHz, {field_str}")
+                # logger.debug(f"For {self.name_tracker.context['split']}:{self.name_tracker.context['sim_num']}, Getting noise map for {freq} GHz, {field_str}")
                 sd_map = self.in_noise_cache.read()
                 noise_map = make_random_noise_map(sd_map, noise_seed, center_frequency)
                 return noise_map
