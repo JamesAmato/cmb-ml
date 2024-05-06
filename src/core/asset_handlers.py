41433e03c0e509a84b537b295e7a40eb6dcdde54
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2024-05-05 19:15:37 -0500

    shorter HealpyMapTemp and ManyMapsTemp

--- src/core/asset_handlers.py
+++ src/core/asset_handlers.py
@@ -62,6 +62,13 @@ class HealpyMap(GenericHandler):
         _make_directories(path)
         hp.write_map(path, data, dtype=data[0].dtype, overwrite=True)
 
+class HealpyMapTemp(HealpyMap):
+    def write(self, path: Union[Path, str], cmb_map):
+        # Note the difference is the addition of map_unit
+        map_unit = str(cmb_map.unit)
+        path = Path(path)
+        _make_directories(path)
+        hp.write_map(path, m=cmb_map, column_units=map_unit, dtype=cmb_map.dtype, overwrite=True)
 
 class ManyHealpyMaps(GenericHandler):
     def __init__(self, experiment: ExperimentParameters) -> None:
@@ -86,6 +93,21 @@ class ManyHealpyMaps(GenericHandler):
             this_path = path.parent / fn
             self.handler.write(this_path, map_to_write)
 
+class ManyHealpyMapsTemp(ManyHealpyMaps):
+    def __init__(self, experiment: ExperimentParameters) -> None:
+        super().__init__(experiment)
+        self.handler: GenericHandler = HealpyMapTemp(experiment)
+
+    def write(self, path: Union[Path, str], data: Dict[str, np.ndarray]) -> None:
+        path = Path(path)
+        for det, map_to_write in data.items():
+            # Note the difference is the addition of... the list check.
+            if isinstance(map_to_write, list):
+                map_to_write = np.stack(map_to_write, axis=0)
+            fn_template = path.name
+            fn = fn_template.format(det=det)
+            this_path = path.parent / fn
+            self.handler.write(this_path, map_to_write)
 
 class Config(GenericHandler):
     def read(self, path: Path) -> Dict:
@@ -100,66 +122,6 @@ class Config(GenericHandler):
         unnumpy_data = _convert_numpy(data)
         with open(path, 'w') as outfile:
             yaml.dump(unnumpy_data, outfile, default_flow_style=False)
-
-
-class HealpyMapTemp(GenericHandler):
-    def read(self, path: Union[Path, str]):
-        path = Path(path)
-        map_fields = self.experiment.map_fields_tuple
-        try:
-            this_map: np.ndarray = hp.read_map(path, field=map_fields)
-        except IndexError as e:
-            if len(map_fields) > 1:
-                # logger.warning("Defaulting to reading a single field from the file. The 857 and 545 maps have no polarization information. Consider suppressing this warning if running a large run.")
-                map_fields = tuple([0])
-                this_map = hp.read_map(path, field=map_fields)
-            else:
-                raise e
-        except FileNotFoundError as e:
-            raise FileNotFoundError(f"This map file cannot be found: {path}")
-        # When healpy reads a single map that I've generated with simulations,
-        #    the bite order is Big-Endian instead of native ('>' instead of '=')
-        if this_map.dtype.byteorder == '>':
-            # The byteswap() method swaps the byte order of the array elements
-            # The newbyteorder() method changes the dtype to native byte order without changing the actual data
-            this_map = this_map.byteswap().newbyteorder()
-        if len(map_fields) == 1:
-            this_map = this_map.reshape(1, -1)
-        if self.experiment.precision == "float":
-            this_map = this_map.astype(np.float32)
-        return this_map
-
-    def write(self, path: Union[Path, str], cmb_map):
-        map_unit = str(cmb_map.unit)
-        path = Path(path)
-        _make_directories(path)
-        hp.write_map(path, m=cmb_map, column_units=map_unit, dtype=cmb_map.dtype, overwrite=True)
-
-class ManyHealpyMapsTemp(GenericHandler):
-    def __init__(self, experiment: ExperimentParameters) -> None:
-        super().__init__(experiment)
-        self.handler: GenericHandler = HealpyMapTemp(experiment)
-
-    def read(self, path: Union[Path, str]) -> Dict[str, np.ndarray]:
-        path = Path(path)
-        maps = {}
-        for det in self.experiment.detector_freqs:
-            fn_template = path.name
-            fn = fn_template.format(det=det)
-            this_path = path.parent / fn
-            maps[det] = self.handler.read(this_path)
-        return maps
-
-    def write(self, path: Union[Path, str], data: Dict[str, np.ndarray]) -> None:
-        path = Path(path)
-        for det, map_to_write in data.items():
-            if isinstance(map_to_write, list):
-                map_to_write = np.stack(map_to_write, axis=0)
-            fn_template = path.name
-            fn = fn_template.format(det=det)
-            this_path = path.parent / fn
-            self.handler.write(this_path, map_to_write)
-
 
 def _convert_numpy(obj: Union[Dict[str, Any], List[Any], np.generic]) -> Any:
     # Recursive function
