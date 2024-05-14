f90cdd772d604bbd56991e04389510c1c5233479
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2024-05-14 12:41:46 -0500

    updated split to have separate, properly defined iterator

--- src/core/split.py
+++ src/core/split.py
@@ -1,12 +1,31 @@
 class Split:
-    def __init__(self, name, cfg) -> None:
+    def __init__(self, name, split_cfg):
         self.name = name
-        self.n_sims = cfg.n_sims
-        self.ps_fidu_fixed = cfg.get("ps_fidu_fixed", None)
+        self.n_sims = split_cfg.n_sims
+        self.ps_fidu_fixed = split_cfg.get("ps_fidu_fixed", None)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
 
     def iter_sims(self):
-        for i in range(self.n_sims):
-            yield i
+        return SplIterator(self)
+
+
+class SplIterator:
+    def __init__(self, split):
+        self.split = split
+        self.current_sim = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current_sim < self.split.n_sims:
+            result = self.current_sim
+            self.current_sim += 1
+            return result
+        else:
+            raise StopIteration
+
+    def __len__(self):
+        return self.split.n_sims
