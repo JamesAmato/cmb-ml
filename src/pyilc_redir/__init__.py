9ca46344ad8aeebe4121b6f4ee604e5bbdcdf8e3
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2024-05-21 12:58:33 -0500

    update for running on Markov

--- src/pyilc_redir/__init__.py
+++ src/pyilc_redir/__init__.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append('/home/jim/VSCode_Projects_SPACE/no_mods/pyilc/pyilc')
+sys.path.append('/shared/code/pyilc/pyilc')
 
 from input import ILCInfo
 from wavelets import Wavelets, wavelet_ILC, harmonic_ILC
