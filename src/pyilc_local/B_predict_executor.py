9e61d70e818cdec55639d5cbf20410e60221b980
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2024-05-21 13:24:37 -0500

    changed logging, added tqdm, tidied imports

--- src/pyilc_local/B_predict_executor.py
+++ src/pyilc_local/B_predict_executor.py
@@ -1,15 +1,16 @@
-from typing import List, Dict
 import logging
 
-import numpy as np
+from tqdm import tqdm
 
 from omegaconf import DictConfig
 
 from core import BaseStageExecutor, Split, Asset
-from .make_pyilc_config import ILCConfigMaker
-from .qtable_handler import QTableHandler  # Import needed to register QTableHandler
-from pyilc_redir.pyilc_wrapper import run_ilc
+from core.asset_handlers.asset_handlers_base import Config, EmptyHandler # Import for typing hint
+from core.asset_handlers.healpy_map_handler import HealpyMap             # Import for typing hint
+from .qtable_handler import QTableHandler                                # Import needed to register QTableHandler
 from utils import make_instrument, Instrument
+from .make_pyilc_config import ILCConfigMaker
+from pyilc_redir.pyilc_wrapper import run_ilc
 from utils.suppress_print import SuppressPrint
 
 
@@ -24,9 +25,15 @@ class PredictionExecutor(BaseStageExecutor):
         self.out_config: Asset = self.assets_out["config_file"]
         self.out_model: Asset = self.assets_out["model"]
         self.out_cmb_asset: Asset = self.assets_out["cmb_map"]
+        out_config_handler: Config
+        out_model_handler: EmptyHandler
+        out_cmb_map_handler: HealpyMap  # Switch to EmptyHandler?
 
         self.in_obs_assets: Asset = self.assets_in["obs_maps"]
         self.in_planck_deltabandpass: Asset = self.assets_in["planck_deltabandpass"]
+        in_obs_handler: HealpyMap  # Switch to EmptyHandler?
+        in_planck_deltabandpass_handler: QTableHandler
+
         in_det_table: Asset = self.assets_in['planck_deltabandpass']
         # with self.name_tracker.set_context("src_root", cfg.local_system.assets_dir):
         #     planck_bandpass = self.in_planck_deltabandpass.read()
@@ -44,7 +51,7 @@ class PredictionExecutor(BaseStageExecutor):
     def process_split(self, 
                       split: Split) -> None:
         logger.info(f"Executing PredictExecutor process_split() for split: {split.name}.")
-        for sim in split.iter_sims():
+        for sim in tqdm(split.iter_sims()):
             with self.name_tracker.set_context("sim_num", sim):
                 self.process_sim()
 
@@ -62,10 +69,10 @@ class PredictionExecutor(BaseStageExecutor):
         cfg_dict = self.model_cfg_maker.make_config(output_path=working_path,
                                                     input_paths=input_paths)
         self.out_config.write(data=cfg_dict)
-        logger.info("Running PyILC Code...")
+        # logger.debug("Running PyILC Code...")
         with SuppressPrint():
             run_ilc(self.out_config.path)
-        logger.info("Moving resulting map.")
+        # logger.debug("Moving resulting map.")
         self.move_result()
 
     def move_result(self):
