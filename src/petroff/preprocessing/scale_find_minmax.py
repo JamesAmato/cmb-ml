fix C_check_transforms - broadcasting caused bad math

src/petroff/preprocessing/scale_find_minmax.py
@@ -8,7 +8,7 @@ def min_max_scale(data, min_v:torch.tensor, max_v:torch.tensor):
     return (data - min_v) / (max_v - min_v)
 
 
-def min_max_unscale(data, min_v, max_v):
+def min_max_unscale(data, min_v:torch.tensor, max_v:torch.tensor):
     return data * (max_v - min_v) + min_v
 
 